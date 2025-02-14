47deaf8f21f94178cc226609a1b15c69c8407d9f
twarnking <twarnking@student.ethz.ch>
2025-02-14 23:08:56 +0100

    check if malloc works

--- simple_examples/vector_addition/vecAdd.cu
+++ simple_examples/vector_addition/vecAdd.cu
@@ -1,1 +1,21 @@
-# good evening
+#include <iostream>
+#include <cuda_runtime.h>
+
+
+int main(){
+  int n = 1024;
+  float* h_a, * h_b, * h_c;
+  float* d_a, * d_b, *d_c;
+
+  // create the host memory
+  h_a = new float[n];
+  h_b = new float[n];
+  h_c = new float[n];
+
+
+  // create the device memory
+  cudaMalloc(&d_a, n * sizeof(float));
+  cudaMalloc(&d_b, n * sizeof(float));
+  
+
+}
